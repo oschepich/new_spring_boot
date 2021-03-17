4ac1717aa4bd6d41a5f7f7f37d989ba3bdfe5f62
oschepich <oschepich@list.ru>
2021-03-17 18:33:34 +0300

    3.1.1 на проверку 17.03.21

--- src/main/java/com/oschepich/spring_boot/new_spring_boot/config/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-//package com.oschepich.spring_boot.new_spring_boot.config;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-//    //пустой класс, использующийся для резистрации модуля в спринг-контейнере
-//}
